49f432618b4a2ff66dbe02c7c5763bfb7e2edd11
ericellb <ericellb@gmail.com>
2019-10-11 17:52:09 -0400

    Updates

---
+++ precache-manifest.4338017df0a1ef6d72e5f9ff4c229464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7decb4973b16b630efa32db94c3b4d",
+    "url": "/Simple-Weather/index.html"
+  },
+  {
+    "revision": "a6b6b837a73ade76bc63",
+    "url": "/Simple-Weather/static/css/main.6ddeb495.chunk.css"
+  },
+  {
+    "revision": "a5a5e74800baa8081ed2",
+    "url": "/Simple-Weather/static/js/2.a31de740.chunk.js"
+  },
+  {
+    "revision": "a6b6b837a73ade76bc63",
+    "url": "/Simple-Weather/static/js/main.d48472d3.chunk.js"
+  },
+  {
+    "revision": "f42038ce0deea6b0a15e",
+    "url": "/Simple-Weather/static/js/runtime-main.567a5c4a.js"
+  }
+]);
