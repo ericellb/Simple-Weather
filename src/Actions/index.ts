7a1a29cf068aafa27e62fedcaa0672d95dc9d651
shmeegie <ericellb@gmail.com>
2019-10-14 18:29:55 -0400

    Memoize fetching with axios

--- src/Actions/index.ts
+++ src/Actions/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
 import { ACTIONS, UpdateCitiesAction, UpdateSelectedCityAction, AddCityAction } from './types';
+import _ from 'lodash';
 
 let WEATHER_KEY = 'e03e2793aa948f9b8e1238d63fbe9d4d';
 
@@ -8,12 +9,12 @@ export const fetchWeatherData = (cityName: string) => async (dispatch: Dispatch)
   // Get Weekly Forecast
   cityName = encodeURIComponent(cityName);
   let weeklyForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${WEATHER_KEY}`;
-  const res = await axios.get(weeklyForecastUrl);
+  const res = await _axios_get(weeklyForecastUrl);
   dispatch({ type: ACTIONS.UPDATE_WEEKLY, payload: res.data.list });
 
   // Get Daily Forecast
   let dailyForecastUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${WEATHER_KEY}`;
-  const res2 = await axios.get(dailyForecastUrl);
+  const res2 = await _axios_get(dailyForecastUrl);
   dispatch({ type: ACTIONS.UPDATE_DAILY, payload: res2.data });
 };
 
@@ -30,4 +31,9 @@ export const addCity = (cityName: string): AddCityAction => ({
 export const updateSelectedCity = (cityName: string): UpdateSelectedCityAction => ({
   type: ACTIONS.UPDATE_SELECTED_CITY,
   payload: cityName
+});
+
+const _axios_get = _.memoize(async (url: string) => {
+  const res = await axios.get(url);
+  return res;
 });
